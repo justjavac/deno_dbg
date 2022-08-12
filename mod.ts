e7bf050b62ae082f87f13331386eb8a5e7bbbe47
迷渡 <justjavac@gmail.com>
2022-08-12 14:55:31 +0800

    first commit

---
+++ mod.ts
@@ -0,0 +1,65 @@
+import { fromFileUrl, relative } from "https://deno.land/std@0.152.0/path/mod.ts";
+import { blue, gray, yellow } from "https://deno.land/std@0.152.0/fmt/colors.ts";
+
+let warn = (msg: string) => {
+  console.warn(msg);
+  warn = () => {};
+};
+
+/**
+ * Prints and returns the value of a given expression for quick and dirty debugging.
+ *
+ * @example
+ *
+ * ```ts
+ * import { dbg } from "https://deno.land/x/dbg/mod.ts";
+ * const a = 2;
+ * const b = dbg(a * 2) + 1;
+ * //        ^-- [examples/main.ts:4:11] 4 (number)
+ * console.assert(b === 5);
+ * ```
+ */
+export function dbg<T>(value: T): T {
+  try {
+    throw new Error();
+  } catch (err) {
+    const stackFrames = (err as Error).stack!.split("\n");
+    let fn = "";
+    let fullFile: string;
+    let line: string;
+    let col: string;
+
+    if (stackFrames.length > 3) {
+      [, fn, fullFile, line, col] = stackFrames[2].match(/at (\S*) \((.*?)\:(\d+)\:(\d+)\)/)!;
+    } else if (stackFrames.length === 3) {
+      [, fullFile, line, col] = stackFrames[2].match(/at (.*?)\:(\d+)\:(\d+)/)!;
+    } else {
+      // unreachable
+      throw new Error("Could not find stack frame");
+    }
+
+    let file = fromFileUrl(fullFile);
+    try {
+      file = relative(".", file);
+    } catch (err) {
+      if ((err as Error).name === "PermissionDenied") {
+        warn(
+          yellow(
+            `No read access to <CWD>, use full path. Or run again with ${
+              blue("--allow-read")
+            }. See https://github.com/justjavac/deno_dbg#read-access`,
+          ),
+        );
+      }
+    }
+
+    console.debug(
+      "%s %s %s",
+      gray(`[${file}:${line}:${col}${fn ? ` (${fn})` : ""}]`),
+      value,
+      blue(`(${typeof value})`),
+    );
+
+    return value;
+  }
+}
